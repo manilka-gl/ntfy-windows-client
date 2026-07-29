6303e8d767243ba7e37dffc9d3dea7bb208fc119
manilka-gl <manilka.gl@gmail.com>
2026-07-29 19:04:44 +0530

    style: apply rustfmt output

--- src/timefmt.rs
+++ src/timefmt.rs
@@ -13,8 +13,7 @@ fn civil_from_days(days_since_epoch: i64) -> (i64, i64, i64) {
     let era = if z >= 0 { z } else { z - 146_096 } / 146_097;
     let day_of_era = z - era * 146_097;
     let year_of_era =
-        (day_of_era - day_of_era / 1_460 + day_of_era / 36_524 - day_of_era / 146_096)
-            / 365;
+        (day_of_era - day_of_era / 1_460 + day_of_era / 36_524 - day_of_era / 146_096) / 365;
     let mut year = year_of_era + era * 400;
     let day_of_year = day_of_era - (365 * year_of_era + year_of_era / 4 - year_of_era / 100);
     let month_prime = (5 * day_of_year + 2) / 153;
