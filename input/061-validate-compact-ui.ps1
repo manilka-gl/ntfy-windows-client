73a05a28e0a69599c090cdb52b001f9d1dc418a8
manilka-gl <manilka.gl@gmail.com>
2026-07-30 14:29:24 +0530

    ci(worker): rerun compact UI validation after formatting fix

---
+++ input/061-validate-compact-ui.ps1
@@ -0,0 +1,22 @@
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+
+$sourcePath = Join-Path $PSScriptRoot '060-validate-compact-ui.ps1'
+$source = [IO.File]::ReadAllText($sourcePath)
+$source = $source.Replace(
+    '[hashtable] $Settings',
+    '[System.Collections.IDictionary] $Settings'
+)
+$source = $source.Replace(
+    '[hashtable] $Window',
+    '[System.Collections.IDictionary] $Window'
+)
+
+$patchedPath = Join-Path $env:RUNNER_TEMP '061-validate-compact-ui-patched.ps1'
+[IO.File]::WriteAllText(
+    $patchedPath,
+    $source,
+    [Text.UTF8Encoding]::new($false)
+)
+
+& $patchedPath
